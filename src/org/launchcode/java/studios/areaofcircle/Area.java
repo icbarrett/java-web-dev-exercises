9039e343580363057a5a88d593b648f862a8f31e
Ingrid Barrett <barrett92@yahoo.com>
2022-01-04 15:07:57 -0500

    Studio updated with bonus mission #2.

--- src/org/launchcode/java/studios/areaofcircle/Area.java
+++ src/org/launchcode/java/studios/areaofcircle/Area.java
@@ -7,16 +7,27 @@ public class Area {
       Scanner input  = new Scanner(System.in);
       System.out.println("Enter the radius:");
       Double radius;
-        if (input.hasNextDouble()){
-            radius = input.nextDouble();
-            if (radius>0){
-                System.out.println("The area of a circle of radius " + radius + " is " + Circle.getArea(radius));
-            } else {
-                System.out.println(radius + " is not a valid number!");
-            }
-        } else {
-            System.out.println("Please enter a valid number!");
-        }
+
+      if (!input.hasNextDouble()) {
+          System.out.println("Not valid number!");
+      } else {
+          radius = input.nextDouble();
+          while (radius < 0){
+              System.out.println(radius + " is not a valid number! Enter a valid number:");
+              radius = input.nextDouble();
+          }
+          System.out.println("The area of a circle of radius " + radius + " is " + Circle.getArea(radius));
+      }
+//        if (input.hasNextDouble()){
+//            radius = input.nextDouble();
+//            if (radius>0){
+//                System.out.println("The area of a circle of radius " + radius + " is " + Circle.getArea(radius));
+//            } else {
+//                System.out.println(radius + " is not a valid number!");
+//            }
+//        } else {
+//            System.out.println("Please enter a valid number!");
+//        }
 
 
     }
